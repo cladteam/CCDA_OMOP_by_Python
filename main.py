33f5c725b765aa36219f3c78647450f2561aea09
chrisroederucdenver <47036053+chrisroederucdenver@users.noreply.github.com>
2024-03-20 10:00:49 -0600

    in-memory output comparisons to avoid needing to write to file system

--- main.py
+++ main.py
@@ -1,36 +1,96 @@
 #!/usr/bin/env python3
 
-import xml.etree.ElementTree as ET
-from pathlib import Path
-import id_map
+# main.py <capture_output>
+#
+# - Runs CCDA to OMOP conversion on a number of listed files for dev/text, retrieved from 
+#   the resources directory.
+# - Compares output to expected output files, in the tests directory.
+#   Comparision does not use Linux diff, because creating a temporary file in Spark environments
+#   can be a hassle. TBD. using difflib for now.
+# - creating new files in Spark environments might make creating expected files difficult. TBD
+#
 
+import xml.etree.ElementTree as ET
+import pathlib
+import difflib
+
+# ToDo: local package
+import id_map
 import location
 import person
 import observation
 import util
 
-input_file_list = [ 'resources/CCDA_CCD_b1_InPatient_v2.xml',
-                    'resources/CCDA_CCD_b1_Ambulatory_v2.xml',
-                    'resources/Inpatient_Encounter_Discharged_to_Rehab_Location(C-CDA2.1).xml',
-                    'resources/170.314b2_AmbulatoryToC.xml',
-                    'resources/ToC_CCDA_CCD_CompGuideSample_FullXML.xml' ]
 
-for input_file in input_file_list:
-    tree = ET.parse(input_file)
+do_capture_output = False
+output_fn = print
+outfile = None
+
+input_filename_list = [
+    'CCDA_CCD_b1_InPatient_v2.xml',
+    'CCDA_CCD_b1_Ambulatory_v2.xml',
+    'Inpatient_Encounter_Discharged_to_Rehab_Location(C-CDA2.1).xml',
+    '170.314b2_AmbulatoryToC.xml',
+    'ToC_CCDA_CCD_CompGuideSample_FullXML.xml' ]
+
+expected_text_file_list = [
+    'CCDA_CCD_b1_InPatient_v2.txt' ,
+    'CCDA_CCD_b1_Ambulatory_v2.txt' ]
+
+
+
+file_num=0;
+for input_filename in input_filename_list:
+    tree = ET.parse('resources/' + input_filename)
+
+    if do_capture_output:
+        output_filename =  input_filename[0:(len(input_filename) -4)] + '.txt'
+        outfile = open('output/' + output_filename, 'w', encoding='utf-8')
+        def capture_output(out_thing):
+            actual_text = ""
+            actual_text += str(out_thing)
+            outfile.write(str(out_thing) + '\n')
+            return actual_text
+        output_fn = capture_output
+    else:
+        def capture_output(out_thing):
+            ##print(out_thing)
+            actual_text = ""
+            actual_text += str(out_thing)
+            return actual_text
+        output_fn = capture_output
+
+
     loc =  location.convert(tree)
 
-    print(f"==================================== {input_file} ===")
     if util.check_CCD_document_type(tree):
+        # Convert
         target = {  
              'location': loc,
              'person': person.convert(tree) ,
-             'observation': observation.convert(tree) 
-               }
-        print(target['location'])
-        print(target['person'])
+             'observation': observation.convert(tree) }
+        output_fn(target['location'])
+        output_fn(target['person'])
         for obs in target['observation']:
-            print(obs)
+            output_fn(obs)
+
+        # Compare
+        actual_string_list = output_fn('')
+        expected_text = pathlib.Path('tests/' + expected_text_file_list[file_num]).read_text()
+        expected_string_list = [expected_text]
+        diff_gen = difflib.context_diff(actual_string_list, expected_string_list,
+                                        fromfile="expected", tofile="actual")
+        try:
+            for difference in diffs:
+                print(difference)
+            print(f"ERROR:Differences found for {input_filename}:")
+        except:
+            print(f"INFO:No Differences for {input_filename}:")
     else:
-        print(f"wrong doc type boss {input_file}")
+        print(f"ERROR:wrong doc type boss {input_filename}")
+
+    file_num += 1
 
 
+if do_capture_output:
+    close(outfile)
