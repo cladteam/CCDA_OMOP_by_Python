fc7b7d37018e585902b201eb308d016276587280
chrisroederucdenver <47036053+chrisroederucdenver@users.noreply.github.com>
2024-03-21 12:24:49 -0600

    catch observations with missing value, report

--- observation.py
+++ observation.py
@@ -43,35 +43,41 @@ def convert(tree):
     dest = list(range(len(results_observations)))
     i = 0
     for obs in results_observations:
-        observation_id = obs.find("id", ns).attrib['root']
-        # observation_id = obs.find("id", ns).attrib['extension']
-
-        observation_code = obs.find("code", ns)
-        observation_concept_id = vocab_map_file.map_hl7_to_omop(
-            observation_code.attrib['codeSystem'], observation_code.attrib['code'])
-
-        observation_date_string = obs.find("effectiveTime", ns).attrib['value']
-        observation_date = util.convert_date(observation_date_string)
-
-        observation_value = obs.find("value", ns)
-        observation_value_value = observation_value.attrib['value']
-        observation_value_type = observation_value.attrib['{' + ns['xsi'] + '}type']
-        observation_value_unit = observation_value.attrib['unit']
-
-        dest[i] = create()
-        dest[i]['observation_id'] = observation_id
-        dest[i]['person_id'] = person_id
-        dest[i]['observation_concept_id'] = observation_concept_id
-        dest[i]['observation_date'] = observation_date
-        # observation value TYPE???  #############
-        if observation_value_type == 'PQ':
-            dest[i]['value_as_number'] = observation_value_value
-        elif observation_value_type == 'ST':
-            dest[i]['value_as_number'] = None
-            dest[i]['value_as_string'] = observation_value
-        dest[i]['value_as_concept_id'] = None
-        dest[i]['provider_id'] = None  # ###############
-        i += 1
+        try:
+            template_id = obs.find("templateId", ns).attrib['root']
+            observation_id = obs.find("id", ns).attrib['root']
+            if template_id != '2.16.840.1.113883.10.20.22.4.2':
+                print(f"INFO: observation template:{template_id} id:{observation_id}")
+            # observation_id = obs.find("id", ns).attrib['extension']
+    
+            observation_code = obs.find("code", ns)
+            observation_concept_id = vocab_map_file.map_hl7_to_omop(
+                observation_code.attrib['codeSystem'], observation_code.attrib['code'])
+    
+            observation_date_string = obs.find("effectiveTime", ns).attrib['value']
+            observation_date = util.convert_date(observation_date_string)
+    
+            observation_value = obs.find("value", ns)
+            observation_value_value = observation_value.attrib['value']
+            observation_value_type = observation_value.attrib['{' + ns['xsi'] + '}type']
+            observation_value_unit = observation_value.attrib['unit']
+    
+            dest[i] = create()
+            dest[i]['observation_id'] = observation_id
+            dest[i]['person_id'] = person_id
+            dest[i]['observation_concept_id'] = observation_concept_id
+            dest[i]['observation_date'] = observation_date
+            # observation value TYPE???  #############
+            if observation_value_type == 'PQ':
+                dest[i]['value_as_number'] = observation_value_value
+            elif observation_value_type == 'ST':
+                dest[i]['value_as_number'] = None
+                dest[i]['value_as_string'] = observation_value
+            dest[i]['value_as_concept_id'] = None
+            dest[i]['provider_id'] = None  # ###############
+            i += 1
+        except KeyError as kex:
+            print(f"WARN: KeyError on observation with id {observation_id} and tempalteId {template_id}, skipped missing key:", kex)
 
     return dest
 
