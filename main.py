354e1adf36ce78d48f9b71e7df726611f0b2cc46
Chris Roeder <47036053+chrisroederucdenver@users.noreply.github.com>
2024-06-05 12:45:55 -0600

    remove TODO

--- main.py
+++ main.py
@@ -37,8 +37,9 @@ input_filename_list = [
     '170.314b2_AmbulatoryToC.xml'
 ]
 
-input_section_filename_list = [  # TODO this one fails because of
-    # checks for things like doctype and an address or patient
+input_section_filename_list = [  
+    # Fails because of checks for things like doctype 
+    # and an address or patient
     'Inpatient_Encounter_Discharged_to_Rehab_Location(C-CDA2.1).xml'
 ]
 
