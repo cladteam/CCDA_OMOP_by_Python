3fbd9f7fdca63c036bd033f0615e3761fc20e546
Chris Roeder <47036053+chrisroederucdenver@users.noreply.github.com>
2024-06-07 12:47:43 -0600

    lint

--- basic_example.py
+++ basic_example.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
 
+"""
+    A basic example for parsing an XML file, fetching some attribute values,
+    and converting them to an OMOP concept_id.
+    Uses some support for keeping the OMOP concept table in Spark.
+"""
+
 import xml.etree.ElementTree as ET
 from util import vocab_spark
 from util.xml_ns import ns
 from util import spark_util
 
-input_filename = 'resources/CCDA_CCD_b1_InPatient_v2.xml'
+INPUT_FILENAME = 'resources/CCDA_CCD_b1_InPatient_v2.xml'
 
-# INIT SPARK for the 
+# INIT SPARK
 spark_util_object = spark_util.SparkUtil()
 
 # PARSE/LOAD
-tree = ET.parse(input_filename)
+tree = ET.parse(INPUT_FILENAME)
 
 # FIND DATA
-observations = tree.findall("./component/structuredBody/component/section/entry/organizer/component/observation", ns)
+observations = tree.findall(
+    "./component/structuredBody/component/section/entry/organizer/component/observation", ns)
 
 for observation in observations:
     observation_code = observation.find("code", ns)
@@ -22,7 +29,8 @@ for observation in observations:
     concept_code = observation_code.attrib['code']
 
     # CONVERT TO OMOP
-    observation_concept_id = vocab_spark.map_hl7_to_omop(vocabulary_id, 
+    observation_concept_id = vocab_spark.map_hl7_to_omop(
+        vocabulary_id,
         concept_code)
 
     # OUTPUT
