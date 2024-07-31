adbcf1270d14bc4b428b6057c6386d2faf723f91
Chris Roeder <47036053+chrisroederucdenver@users.noreply.github.com>
2024-07-31 09:53:22 -0600

    added filters in compare_correct.sh for CSV versions of metadata (they aren't output data), updated correct_output with additional float columns in recent config updates

--- prototype_2/bin/compare_correct.sh
+++ prototype_2/bin/compare_correct.sh
@@ -2,6 +2,8 @@
 
 for file in *.csv
 do
-    echo $file
-    diff correct_output/$file $file
+    if [[  $file != 'metadata.csv' && $file != 'metadata_w_intermediate.csv' ]]; then
+        echo $file
+        diff correct_output/$file $file
+    fi
 done
