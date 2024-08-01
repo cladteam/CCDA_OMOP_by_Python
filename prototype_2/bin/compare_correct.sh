3b2f5859ecc049c38f87644e7a8368de38f87177
Chris Roeder <47036053+chrisroederucdenver@users.noreply.github.com>
2024-08-01 13:54:04 -0600

    updated correct files with ones that contain the root path, tweaked the test script

--- prototype_2/bin/compare_correct.sh
+++ prototype_2/bin/compare_correct.sh
@@ -5,5 +5,8 @@ do
     if [[  $file != 'metadata.csv' && $file != 'metadata_w_intermediate.csv' ]]; then
         echo $file
         diff correct_output/$file $file
+        if [[ $? == 0 ]]; then
+            echo "OK"
+        fi
     fi
 done
