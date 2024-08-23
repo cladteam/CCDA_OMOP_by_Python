d7d61c74a545d996e6c2312d26d4d994393430df
Chris Roeder <47036053+chrisroederucdenver@users.noreply.github.com>
2024-08-22 18:38:09 -0600

    added better output to compare script

--- bin/compare_correct.sh
+++ bin/compare_correct.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
 err_count=0
+file_count=0
 for file in output/*.csv
 do
     base_file=$(basename $file)
     echo ""
     echo "$file  <-->  prototype_2/correct_output/$base_file" 
     diff $file prototype_2/correct_output/$base_file 
-    err_count=$(( $err_count + $? ))
+    errval=$?
+    if [[ $errval > 0 ]] ; then
+        err_count=$(( $err_count + 1 ))
+        echo "$file produced an error $errval"
+    fi
+    file_count=$(( $file_count + 1))
 done
 
-echo "counted $err_count errors"
+echo "counted $err_count errors of $file_count files"
 
 exit $err_count
