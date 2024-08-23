754b410caad48102f11757b7f18e4cf53f908349
Chris Roeder <47036053+chrisroederucdenver@users.noreply.github.com>
2024-08-22 18:00:06 -0600

    correct error handling of the compare_correct.sh script so github action fails if the files aren't what is expected

--- bin/compare_correct.sh
+++ bin/compare_correct.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+err_count=0
 for file in output/*.csv
 do
     base_file=$(basename $file)
     echo ""
     echo "$file  <-->  prototype_2/correct_output/$base_file" 
     diff $file prototype_2/correct_output/$base_file 
-    if [[ $? == 0 ]]; then
-        echo "OK"
-    fi
+    err_count=$(( $err_count + $? ))
 done
+
+echo "counted $err_count errors"
+
+exit $err_count
