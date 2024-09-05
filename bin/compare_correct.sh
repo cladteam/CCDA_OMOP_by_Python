7693bd4a999855913dd5089a7a0dc9ad9a918dfd
Chris Roeder <47036053+chrisroederucdenver@users.noreply.github.com>
2024-09-05 09:57:17 -0600

    added some detail to compare_correct.sh output

--- bin/compare_correct.sh
+++ bin/compare_correct.sh
@@ -6,12 +6,19 @@ for file in output/*.csv
 do
     base_file=$(basename $file)
     echo ""
-    echo "$file  <-->  prototype_2/correct_output/$base_file" 
-    diff $file prototype_2/correct_output/$base_file 
+    echo "$file  <-->  prototype_2/correct_output/$base_file"
+    diff $file prototype_2/correct_output/$base_file
     errval=$?
     if [[ $errval > 0 ]] ; then
         err_count=$(( $err_count + 1 ))
         echo "$file produced an error $errval"
+        echo -n "A head: "
+        head -1 $file
+        echo -n "B head: "
+        head -1 prototype_2/correct_output/$base_file
+    else
+        echo -n  "OK"
+        wc -l $file
     fi
     file_count=$(( $file_count + 1))
 done
