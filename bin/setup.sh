45961401e39bfbddbaa85cde48deb67dd9a37a02
Chris Roeder <27caf55b-22a1-4fd3-a21a-8401c0ae33fd>
2024-09-19 17:22:22 +0000

    Autogenerated commit message at Sep 19, 2024 at 12:05

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,6 +3,10 @@
 # If only to document what's needed...
 # run from  base directory, CCDA_OMOP_by_Python
 
+
+mamba install -y -q lxml
+mamba install -y -q duckdb
+
 mkdir output 2> /dev/null
 mkdir logs 2> /dev/null
 ln -s ../CCDA-data/resources .
