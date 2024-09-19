989ef60320264442e664c27d579b7eaefd08ad3b
Chris Roeder <27caf55b-22a1-4fd3-a21a-8401c0ae33fd>
2024-09-19 17:22:22 +0000

    Autogenerated commit message at Sep 19, 2024 at 12:06

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,8 +3,10 @@
 # If only to document what's needed...
 # run from  base directory, CCDA_OMOP_by_Python
 
-
+echo "installing lxml"
 mamba install -y -q lxml
+
+echo "installing duckdb"
 mamba install -y -q duckdb
 
 mkdir output 2> /dev/null
