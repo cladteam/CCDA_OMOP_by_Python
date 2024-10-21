770ca47c611f314c143f73b993becc57f537f6df
Christophe Roeder <croeder@croeder.com>
2024-10-21 15:55:29 -0600

    added trace output from directory option

--- prototype_2/data_driven_parse.py
+++ prototype_2/data_driven_parse.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 
+# TODO
+# The trace is picking up just observations and encounters without fields below them, where from?
+# resources/CCD-Sample.xml,2.16.840.1.113883.10.20.22.2.22.1,component/structuredBody/component[1]/section/entry[2]/encounter,,FIELD,Visit,care_site_id,,
+# config below:
+#       'element': "hl7:location/hl7:healthCareFacility/hl7:id",
+#            'attribute': "root",
+# ---> buggy rules will show a trace without a value, or from a higher level where this is no value, like on encounter. The values are lower.
+# fix by filtering in the coverage report.
+
 """ Table-Driven ElementTree parsing in Python
 
  This version puts the paths into a data structure and explores using
@@ -725,10 +734,17 @@ def main() :
         df = pd.DataFrame(all_trace_list, columns=['filename', 'template_id','root_xpath',   'element_tag', 'config_type', 'domain', 'omop_field_tag', 'attribute_value', 'attributes'])
         df.to_csv("trace.csv", header=True, index=False)
     elif args.directory is not None:
+        all_traces_list=[]
+        trace_list=[]
         only_files = [f for f in os.listdir(args.directory) if os.path.isfile(os.path.join(args.directory, f))]
         for file in (only_files):
             if file.endswith(".xml"):
-            	process_file(os.path.join(args.directory, file))
+                trace_list = process_file(os.path.join(args.directory, file))
+            all_traces_list.extend(trace_list)
+        df = pd.DataFrame(all_traces_list, 
+                          columns=['filename', 'template_id','root_xpath',   'element_tag', 
+                                   'config_type', 'domain', 'omop_field_tag', 'attribute_value', 'attributes'])
+        df.to_csv("trace.csv", header=True, index=False)
     else:
         logger.error("Did args parse let us  down? Have neither a file, nor a directory.")
 
