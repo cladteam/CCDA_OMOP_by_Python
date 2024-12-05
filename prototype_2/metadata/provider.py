327e6773ab39680108a835925abcb8aaa3f83388
Chris Roeder <27caf55b-22a1-4fd3-a21a-8401c0ae33fd>
2024-12-05 16:52:19 +0000

    Autogenerated commit message at Dec 05, 2024 at 10:42

--- prototype_2/metadata/provider.py
+++ prototype_2/metadata/provider.py
@@ -14,13 +14,40 @@ metadata = {
             'element': ('./hl7:component/hl7:structuredBody/hl7:component/hl7:section/hl7:templateId[@root="2.16.840.1.113883.10.20.22.2.10"]/../'
                         'hl7:entry/hl7:encounter/hl7:performer/hl7:assignedEntity' )
         },
-
+        #   <id extension="444222222" root="2.16.840.1.113883.4.1"/>
+        'provider_id_extension': {
+            'config_type': 'HASH',
+            'element': 'hl7:id',
+            'attribute': "extension",
+        },
+       'provider_id_root': {
+            'config_type': 'HASH',
+            'element': 'hl7:id',
+            'attribute': "root",
+        },
         'provider_id': {
-            'config_type': 'FIELD',
+            'config_type': 'HASH',
+            'fields' : [ 'provider_id_extension', 'provider_id_root']
             'order': 1 ,
+        },
+        'provider_name': { 
+            'config_type': 'None',
+            'order': 2
+        },
+
+        'npi': {
+            'config_type': 'FIELD',
             'element': 'hl7:id[@root="2.16.840.1.113883.4.6"]',
             'attribute': "extension",
-        },#<code code="207QA0505X" displayName="Adult Medicine Physician" codeSystem="2.16.840.1.113883.6.101" codeSystemName="NUCC" />
+            'order': 3,
+        },
+        'dea': {
+            'config_type': 'FIELD',
+            'element': 'hl7:id[@root="2.16.840.1.113883.D.E.A"]',
+            'attribute': "extension",
+            'order': 4,
+        },
+        #<code code="207QA0505X" displayName="Adult Medicine Physician" codeSystem="2.16.840.1.113883.6.101" codeSystemName="NUCC" />
         'specialty_id_code': { 
             'config_type': 'FIELD',
             'element': 'hl7:code',
@@ -39,7 +66,7 @@ metadata = {
     		        'vocabulary_oid': 'specialty_id_codeSystem',
                     'default': 0
     	         },
-            'order': 3
+            'order': 5
         },
     
 
