91e8282e703096081a22ebc76f3eff56f8d86774
Chris Roeder <27caf55b-22a1-4fd3-a21a-8401c0ae33fd>
2024-12-18 22:57:44 +0000

    added a hash for provider_id secondary to the id element

--- prototype_2/metadata/provider.py
+++ prototype_2/metadata/provider.py
@@ -11,7 +11,6 @@ metadata = {
 
         'root': {
             'config_type': 'ROOT',
-           
             'element': ('./hl7:component/hl7:structuredBody/hl7:component/hl7:section/hl7:templateId[@root="2.16.840.1.113883.10.20.22.2.22.1"]/../'
                         'hl7:entry/hl7:encounter/hl7:performer/hl7:assignedEntity' )
         },
@@ -19,17 +18,58 @@ metadata = {
         'provider_id_extension': {
             'config_type': 'FIELD',
             'element': 'hl7:id',
-            'attribute': "extension",
+            'attribute': "extension"
         },
        'provider_id_root': {
             'config_type': 'FIELD',
             'element': 'hl7:id',
-            'attribute': "root",
+            'attribute': "root"
         },
-        'provider_id': {
+        'provider_id_element': {
             'config_type': 'HASH',
-          'fields' : [ 'provider_id_extension', 'provider_id_root'],
-            'order': 1 ,
+            'fields': [ 'provider_id_extension', 'provider_id_root'],
+            'priority' : ['provider_id', 1]
+        },
+        
+        'provider_id_street': {
+            'config_type': 'FIELD',
+            'element': 'hl7:addr/hl7:streetAddressLine',
+            'attribute': "#text"
+        },
+       'provider_id_city': {
+            'config_type': 'FIELD',
+            'element': 'hl7:addr/hl7:city',
+            'attribute': "#text"
+        },
+        'provider_id_state': {
+            'config_type': 'FIELD',
+            'element': 'hl7:addr/hl7:state',
+            'attribute': "#text"
+        },
+        'provider_id_zip': {
+            'config_type': 'FIELD',
+            'element': 'hl7:addr/hl7:postalCode',
+            'attribute': "#text"
+        },
+        'provider_id_given': {
+            'config_type': 'FIELD',
+            'element': 'hl7:assignedPerson/hl7:name/hl7:given',
+            'attribute': "#text"
+        },
+        'provider_id_family': {
+            'config_type': 'FIELD',
+            'element': 'hl7:assignedPerson/hl7:name/hl7:family',
+            'attribute': "#text"
+        },
+        'provider_id_hash': {
+            'config_type': 'HASH',
+            'fields' : [ 'provider_id_street', 'provider_id_city', 'provider_id_state', 'provider_id_zip', 'provider_id_given', 'provider_id_family'],
+            'priority' : ['provider_id', 2]
+        },
+       
+        'provider_id': {
+            'config_type': 'PRIORITY',
+            'order': 1
         },
 
         'provider_name': { 
@@ -41,24 +81,24 @@ metadata = {
             'config_type': 'FIELD',
             'element': 'hl7:id[@root="2.16.840.1.113883.4.6"]',
             'attribute': "extension",
-            'order': 3,
+            'order': 3
         },
         'dea': {
             'config_type': 'FIELD',
             'element': 'hl7:id[@root="2.16.840.1.113883.D.E.A"]', # TODO get the correct OID
             'attribute': "extension",
-            'order': 4,
+            'order': 4
         },
         #<code code="207QA0505X" displayName="Adult Medicine Physician" codeSystem="2.16.840.1.113883.6.101" codeSystemName="NUCC" />
         'specialty_id_code': { 
             'config_type': 'FIELD',
             'element': 'hl7:code',
-            'attribute': "code",
+            'attribute': "code"
         },
         'specialty_id_codeSystem': { 
             'config_type': 'FIELD',
             'element': 'hl7:code',
-            'attribute': "codesystem",
+            'attribute': "codesystem"
         },
         'specialty_id': {
        	    'config_type': 'DERIVED',
@@ -71,19 +111,42 @@ metadata = {
             'order': 5
         },
         
-        'care_site_id_root': {
-            'config_type': 'FIELD', # To be fleshed out using "parse twice"
-            'element': '../../hl7:particpant/hl7:participant[@typeCode="LOC"]/',
-            ## need to collect element name, address if available, telephone if available, and hash m
+        # hl7:encounter/hl7:participant/hl7:participantRole
+        'care_site_id': { 
+            'config_type': 'FIELD',
+            'element': '../../hl7:participant/hl7:participantRole/hl7:id',
             'attribute': "root",
             'order': 6
         },
+  
+        'year_of_birth': {
+            'config_type': None,
+            'order' :7
+        },
+        'gender_concept_id': {
+            'config_type': None,
+            'order' :8
+        },
+        'provider_source_value': {
+            'config_type': None,
+            'order' :9
+        },
+        'specialty_source_value': {
+            'config_type': None,
+            'order' :10
+        },
+        'specialty_source_concept_id': {
+            'config_type': None,
+            'order' :11
+        }, 
+        'gender_source_value': {
+            'config_type': None,
+            'order' :12
+        },
+        'gender_source_concept_id': {
+            'config_type': None,
+            'order' :13
+        }
+    },
     
-
-        #'location_id': {
-        #    'config_type': 'HASH',
-       #     'fields' : [ 'address_1', 'city', 'state', 'zip'  ],
-        #    'order': 4
-       # },
-     }
 }
