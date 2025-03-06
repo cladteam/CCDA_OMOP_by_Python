b76e58142d15f59281402f8b2620054d09e79734
Chris Roeder <27caf55b-22a1-4fd3-a21a-8401c0ae33fd>
2025-03-06 22:10:06 +0000

    added tests to bin/test.sh, fixed column in codemap_xwalk

--- bin/test.sh
+++ bin/test.sh
@@ -2,24 +2,25 @@
 set -eou pipefail
 
 echo test_value_transformations
-python3 -m unittest prototype_2.test.test_value_transformations
+ python3 -m unittest prototype_2.test.test_value_transformations
 echo "-----------------"
 
 echo test_value_transformations_valueset
-python3 -m unittest prototype_2.test.test_value_transformations_valueset
+ python3 -m unittest prototype_2.test.test_value_transformations_valueset
 echo "-----------------"
 
 echo test_value_transformations_codemap
-python3 -m unittest prototype_2.test.test_value_transformations_codemap
+ python3 -m unittest prototype_2.test.test_value_transformations_codemap
 echo "-----------------"
 
 echo test_field_types
-python3 -m unittest prototype_2.test.test_field_types
+ python3 -m unittest prototype_2.test.test_field_types
 echo "-----------------"
 
-echo compare_maps
-python3 -m unittest prototype_2.test.compare_maps
-echo "-----------------"
+# obsolete?
+#echo compare_maps
+#python3 -m unittest prototype_2.test.compare_maps
+#echo "-----------------"
 
 echo test_value_transformations_concat
 python3 -m  unittest prototype_2.test.test_value_transformations_concat
@@ -28,6 +29,19 @@ echo "-----------------"
 echo test_concept_lookups_codemap
 python3 -m  unittest prototype_2.test.test_concept_lookups_codemap
 echo "------------------"
+
+echo test_timezone_comparisons
+python3 -m  unittest prototype_2.test.test_timezone_comparisons
+echo "------------------"
+
+echo test_type_handling
+python3 -m  unittest prototype_2.test.test_type_handling
+echo "------------------"
+
+echo test_visit_FK_reconciliation
+python3 -m  unittest prototype_2.test.test_visit_FK_reconciliation
+echo "------------------"
+
 
 # load into DuckDB for constraint errors
 python3 -m omop.setup_omop
--- prototype_2/test/compare_maps.py
+++ prototype_2/test/compare_maps.py
@@ -8,7 +8,9 @@ from .. import ccda_value_set_mapping_table_dataset
 
 codemap_exceptions = [
     # different mapping, but OK
-    '409586006', '4668005', '99212', '99213'
+    '409586006', 
+    #'4668005', 
+    '99212', '99213'
 ]
 
 
@@ -29,10 +31,10 @@ codemap_exceptions = [
 failures = [
     #2.16.840.1.113883.6.101, NUCC  -- all to 0
     '163W00000X', '207QA0505X', '207R00000X', '207RC0000X',
-    '208D00000X', '261QP2300X' #?
+    '208D00000X', '261QP2300X', 
 
     #2.16.840.1.113883.6.238, PHIN VADS -- all to 0
-    '2076-8', '2106-3', '2186-5'
+    '2076-8', '2106-3', '2186-5',
 
     #2.16.840.1.113883.6.96, SNOMED 
     '95971004' # maps to 0
@@ -52,16 +54,6 @@ class Compare_maps(unittest.TestCase):
             expected_concept_id = str(source_row['concept_id'])
             expected_domain_id = source_row['domain_id']
             source_count += 1
-            #print(f" {index} {source_row}")
-            ### print(f" {oid}, {concept_code} --> {expected_concept_id} {expected_domain_id}")
-
-            
-            #print(f" oid:{source_row['vocab_oid']}  concept_code:{source_row['source_concept_code']}")
-            #print( (f"source_concept_id:{source_row['source_concept_id']} "
-            #        f"target_concept_id:{source_row['target_concept_id']} "
-            #        f"target_domain_id:{source_row['target_domain_id']} ") )
-            #vocabulary_oid = source_row['vocab_oid']
-            #concept_code = source_row['source_concept_code']
 
             
             # Value Set
@@ -73,6 +65,8 @@ class Compare_maps(unittest.TestCase):
                 mapped_concept_id = df['target_concept_id'].iloc[0]
                 mapped_domain_id = df['target_domain_id'].iloc[0]
                 print(f"FOUND valueset mapped concept:{mapped_concept_id} ")
+                if (mapped_concept_id != expected_concept_id):
+                    print(f"Error on {oid} {concept_code}")
                 self.assertEqual(mapped_concept_id, expected_concept_id)
                 self.assertEqual(mapped_domain_id, expected_domain_id)
                 if mapped_concept_id == expected_concept_id:
@@ -84,7 +78,7 @@ class Compare_maps(unittest.TestCase):
                            f"correctly code:{concept_code} to id: {mapped_concept_id}"))
             else:
                 # Code Map
-                df = codemap_xwalk[ (codemap_xwalk['vocab_oid'] == oid) &
+                df = codemap_xwalk[ (codemap_xwalk['src_vocab_code_system'] == oid) &
                             (codemap_xwalk['src_code']  == concept_code) ]
                 if df['target_concept_id'].size > 0:
                     codemap_found_count += 1
@@ -96,7 +90,7 @@ class Compare_maps(unittest.TestCase):
                         if concept_code in failures:
                             print(f"failure was successfully mapped by thevalue set table {concept_code}")
                     else: 
-                        print(f"{oid}, {concept_code} --> {expected_concept_id} {expected_domain_id}")
+                        print(f"OID:{oid}, code:{concept_code} --> concept_id:{expected_concept_id} domain_id{expected_domain_id}")
                         print(( f"    NOT FOUND codemap code mapped:\"{mapped_concept_id}\" "
                                 f"domain:\"{mapped_domain_id}\" "))
                         if concept_code in failures:
