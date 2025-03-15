446b82a7ca4c16f85b480a65ec49f526eb5d74f4
Chris Roeder <27caf55b-22a1-4fd3-a21a-8401c0ae33fd>
2025-03-15 18:20:48 +0000

    Autogenerated commit message at Mar 15, 2025 at 12:38

--- bin/run_dataset.sh
+++ bin/run_dataset.sh
@@ -14,7 +14,6 @@ rm -f logs/*Provider*
 rm -f logs/*Procedure*
 rm -f logs/*Medicat*
 rm -f logs/log_config*
-ls logs
 rm -f logs/*
 rm -f output/*
 
@@ -22,11 +21,11 @@ rm -f output/*
 #python3 -m prototype_2.layer_datasets -ds ccda_response_files -l 3 -s 6 -x
 
 # base run: 1000 files
-date > batch_1.txt
-git status >> batch_1.txt
-echo "STARTING" >> batch_1.txt
-python3 -m prototype_2.layer_datasets -ds ccda_response_files -l 30 -s 0 -x  >> batch_1.txt
-date >> batch_1.txt
+date > /home/user/batch_1.txt
+git status >> /home/user/batch_1.txt
+echo "STARTING" >> /home/user/batch_1.txt
+python3 -m prototype_2.layer_datasets -ds ccda_response_files -l 30 -s 0 -x  >> /home/user/batch_1.txt
+date >> /home/user/batch_1.txt
 
 
 
