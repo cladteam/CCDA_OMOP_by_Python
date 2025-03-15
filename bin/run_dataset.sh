361e655d5c3b3441ceb70d25c4a743f71790fe0d
Chris Roeder <27caf55b-22a1-4fd3-a21a-8401c0ae33fd>
2025-03-15 18:20:48 +0000

    logging from bash removed

--- bin/run_dataset.sh
+++ bin/run_dataset.sh
@@ -21,11 +21,17 @@ rm -f output/*
 #python3 -m prototype_2.layer_datasets -ds ccda_response_files -l 3 -s 6 -x
 
 # base run: 1000 files
-date > /home/user/batch_1.txt
-git status >> /home/user/batch_1.txt
-echo "STARTING" >> /home/user/batch_1.txt
-python3 -m prototype_2.layer_datasets -ds ccda_response_files -l 30 -s 0 -x  >> /home/user/batch_1.txt
-date >> /home/user/batch_1.txt
+#date > /home/user/batch_1.txt
+#git status >> /home/user/batch_1.txt
+#echo "STARTING" >> /home/user/batch_1.txt
+#python3 -m prototype_2.layer_datasets -ds ccda_response_files -l 20 -s 0 -x   >> /home/user/batch_1.txt
+#date >> /home/user/batch_1.txt
+
+date
+git status 
+echo "STARTING" 
+python3 -m prototype_2.layer_datasets -ds ccda_response_files -l 3000 -s 0 -x 
+date
 
 
 
